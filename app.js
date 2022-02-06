f29f87bd6acd3fa701be770384b08a5132b236b8
jhpark <parkjh1@mih.co.kr>
2022-02-06 22:00:41 +0900

    routes 분리

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
+"use strict";
+
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 
-//html 하드코딩 
-// app.get("/index",(req, res)=>{
-//     res.send(`
-//     <!DOCTYPE html>
-//     <html lang="ko">
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-//     </head>
-//     <body>
-//         <input type="text" placeholder="ID"><br>
-//         <input type="text" placeholder="PASSWORD"><br>
-//         <button>LOGIN</button>
-//     </body>
-//     </html>
-//     `
-//     );
-// });
+const home = require("./routes/home");
 
 //app settings
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("home/index");
-});
-
-app.get("/login", (req, res)=>{
-    res.render("home/login");
-});
-
+app.use("/", home); 
+// use 미들웨어를 등록 
 
 app.listen(3000, () =>{
     console.log("server open");
