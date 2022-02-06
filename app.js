26dc38519c2c34f5797a677f8ed9409ce608f09d
jhpark <parkjh1@mih.co.kr>
2022-02-06 21:48:42 +0900

    view 분리

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
+//html 하드코딩 
+// app.get("/index",(req, res)=>{
+//     res.send(`
+//     <!DOCTYPE html>
+//     <html lang="ko">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     </head>
+//     <body>
+//         <input type="text" placeholder="ID"><br>
+//         <input type="text" placeholder="PASSWORD"><br>
+//         <button>LOGIN</button>
+//     </body>
+//     </html>
+//     `
+//     );
+// });
+
+//app settings
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/",(req,res)=>{
-    res.send("check to route");
+    res.render("home/index");
 });
 
 app.get("/login", (req, res)=>{
-    res.send("check to login");
+    res.render("home/login");
 });
+
 
 app.listen(3000, () =>{
     console.log("server open");
